dcbb8167d20ac218da68bfc0c8941ea870c7108f
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-03-03 20:30:13 +0900

    feat: letを理解

--- variables.js
+++ variables.js
@@ -1,4 +1,16 @@
-const name = "寅松";
+const myname = "寅松";
 const language = "JavaScript";
 
-console.log(name + "、" + language + "の洗礼を受ける。");
+console.log(myname + "、" + language + "の洗礼を受ける。");
+
+let favoriteMusic = "Plazma";
+
+console.log(favoriteMusic + "はいいぞおじさん「" + favoriteMusic + "はいいぞ」");
+
+favoriteMusic = "春雷";
+
+console.log(favoriteMusic + "はいいぞおじさん「" + favoriteMusic + "はいいぞ」");
+
+favoriteMusic = "LADY";
+
+console.log(favoriteMusic + "はいいぞおじさん「" + favoriteMusic + "はいいぞ」");
